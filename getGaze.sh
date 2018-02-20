66a794ec89802e6a866e1571e309faeb7edb6a53
timjzee <timjzee@gmail.com>
2018-02-21 00:00:13 +0100

    Moved files around

--- getGaze.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-wget -r -l1 -np -nd -A "*.gaze" --directory-prefix=/Users/tim/Documents/IFADVcorpus/annotations/source_textgrids/ http://www.fon.hum.uva.nl/IFA-SpokenLanguageCorpora/IFADVcorpus/Annotations/EAF/
--- getPOS.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-wget -r -l1 -np -nd -A "*.pos" --directory-prefix=/Users/tim/Documents/IFADVcorpus/annotations/POS_files/ http://www.fon.hum.uva.nl/IFA-SpokenLanguageCorpora/IFADVcorpus/Annotations/pos/
